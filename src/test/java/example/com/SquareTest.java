9e33a3959ba64945341f5becedd6f1ee43c9f54e
Jacek Borkowski (borkowski.jb) <jacek.borkowski@lingaro.com>
2021-05-25 16:40:06 +0200

    fresh start

--- src/test/java/example/com/SquareTest.java
+++ src/test/java/example/com/SquareTest.java
@@ -3,7 +3,7 @@ package example.com;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class SingleTest {
+public class SquareTest {
     @Test
     void aaa(){
         Assertions.assertTrue(false);
