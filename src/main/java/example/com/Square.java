4ab244f7f9e06d3c00b8ed96815523d07228d4fb
Jacek Borkowski (borkowski.jb) <jacek.borkowski@lingaro.com>
2021-05-25 17:50:47 +0200

    refactor step 2 + reordering of notes

--- src/main/java/example/com/Square.java
+++ src/main/java/example/com/Square.java
@@ -2,16 +2,17 @@ package example.com;
 
 public class Square {
     public void calculate(int n) {
-        if(n == 0){
-            throw new NotAcceptableNumber();
-        }else if(n < 0){
-            throw new NotAcceptableNumber();
+        if(n <= 0){
+            throw new NotAcceptableNumber(n);
         }
 
     }
 
     static class NotAcceptableNumber extends RuntimeException {
 
+        public NotAcceptableNumber(int n) {
+
+        }
     }
 
 }
