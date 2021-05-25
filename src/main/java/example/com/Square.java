ae8200ff9719a8315c6a78c7c852d77206dea339
Jacek Borkowski (borkowski.jb) <jacek.borkowski@lingaro.com>
2021-05-25 18:29:13 +0200

    can store valid inputs

--- src/main/java/example/com/Square.java
+++ src/main/java/example/com/Square.java
@@ -5,13 +5,15 @@ import java.util.Optional;
 public class Square {
 
     public static final int MAX_SIDE = 1000;
+    private final Repository repository;
 
     public Square(Repository repository) {
-
+        this.repository = repository;
     }
 
     public int calculate(int n) {
         Optional<ValidSide> maybeValidSide = ValidSide.of(n);
+        repository.saveSquareSide(n);
         return maybeValidSide.map(Square::square).orElseThrow(()->new NotAcceptableNumber(n));
     }
 
